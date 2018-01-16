394981050c7e992b65e9045d2bf6ac6d589b5143
jukim <jik1423248@gmail.com>
2018-01-16 19:24:51 +0900

    껍데기

--- src/components/App.js
+++ src/components/App.js
@@ -12,16 +12,19 @@ class App extends React.Component {
 				meanN:'뜀박질',
 				meanA:'',
 				meanV:'',
+				isFavorite:'',
 			},{
 				string:'orthodox',
 				meanN:'정통파의 사람',
 				meanA:'정통의',
 				meanV:'',
+				isFavorite:'',
 			},{
 				string:'scrap',
 				meanN:'조각',
 				meanA:'',
 				meanV:'싸우다, 다투다',
+				isFavorite:'',
 			}]
 		}
 
@@ -29,11 +32,11 @@ class App extends React.Component {
 
 
     render(){
-    	console.log(this.state.words)
+    	//console.log(this.state.words)
         return (
         	<div>
             	<AppBar/>
-            	<Tabs/>
+            	<Tabs words = {this.state.words}/>
             </div>
         );
     }
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {Tabs, Tab} from 'material-ui/Tabs';
 
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+
+import {Motion, spring} from 'react-motion';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const styles = {
   wrapper: {
-    padding: '0px 20px',
+
   },
   headline: {
     fontSize: 15,
@@ -16,46 +22,80 @@ const styles = {
   }
 };
 
+const Thing = () => (
+  <Motion defaultStyle={{ opacity: 0 }} style={{ opacity: spring(1) }}>
+    { (style) => <div style={style}>Can you see me?</div> }
+  </Motion>
+)
+
 function handleActive(tab) {
   alert(`A tab with this route property ${tab.props.index} was activated.`);
 }
 
-const TabsExampleSimple = () => (
-  <MuiThemeProvider>
-    <Tabs inkBarStyle={{backgroundColor:'rgb(255, 235, 59)'}}>
-      <Tab label="학습" style={{backgroundColor:'rgb(76,175,80)'}}>
-        <div style={styles.wrapper}>
-          <h2 style={styles.headline}>완전히 기억하고 있는 단어</h2>
-          <h2 style={styles.word}>word</h2>
-        </div>
-      </Tab>
+export default class TabsExampleSimple extends React.Component {
+  constructor(props) {
+    super(props)
 
-      <Tab label="모르는 단어" style={{backgroundColor:'rgb(76,175,80)'}}>
-        <div style={styles.wrapper}>
-          <h2 style={styles.headline}>완전히 기억하고 있는 단어</h2>
-          <h2 style={styles.word}>word</h2>
-        </div>
-      </Tab>
+    this.state = {
+      words:this.props.words
+    }
+  }
 
-      <Tab label="한눈에 보기" style={{backgroundColor:'rgb(76,175,80)'}}>
-        <div style={styles.wrapper}>
-          <h2 style={styles.headline}>완전히 기억하고 있는 단어</h2>
-          <h2 style={styles.word}>word</h2>
-        </div>
-      </Tab>
 
-      <Tab label="암기 현황" style={{backgroundColor:'rgb(76,175,80)'}}>
-        <div style={styles.wrapper}>
-          <h2 style={styles.headline}>완전히 기억하고 있는 단어</h2>
-          <h2 style={styles.word}>word</h2>
-        </div>
-        <div style={styles.wrapper}>
-          <h2 style={styles.headline}>1회 학습한 단어</h2>
-          <h2 style={styles.word}>word</h2>
-        </div>        
-      </Tab>      
-    </Tabs>
-  </MuiThemeProvider>
-);
 
-export default TabsExampleSimple;
+  render() {
+    console.log(this.state)
+    let words = this.state.words.map((memo, i) => {
+                  //if(!memo.isDeleted)
+                  return(
+                    <Toolbar key = {i} style = {{backgroundColor:'white', borderBottom:'1px solid gray'}}>
+                      <ToolbarGroup firstChild={true} style={{padding:'10px'}}>
+                        {memo.string}
+                      </ToolbarGroup>
+                      <ToolbarGroup>
+                        <StarBorder/>
+                        <ActionInfo />
+                        <Thing/>
+                      </ToolbarGroup>                      
+                    </Toolbar>                      
+                  );
+                })
+
+    
+
+    return (
+      <MuiThemeProvider>
+        <Tabs inkBarStyle={{backgroundColor:'rgb(255, 235, 59)'}}>
+          <Tab label="학습" style={{backgroundColor:'rgb(76,175,80)'}}>
+            {words}
+          </Tab>
+
+          <Tab label="모르는 단어" style={{backgroundColor:'rgb(76,175,80)'}}>
+            <div style={styles.wrapper}>
+              <h2 style={styles.headline}>완전히 기억하고 있는 단어</h2>
+              <h2 style={styles.word}>word</h2>
+            </div>
+          </Tab>
+
+          <Tab label="한눈에 보기" style={{backgroundColor:'rgb(76,175,80)'}}>
+            <div style={styles.wrapper}>
+              <h2 style={styles.headline}>완전히 기억하고 있는 단어</h2>
+              <h2 style={styles.word}>word</h2>
+            </div>
+          </Tab>
+
+          <Tab label="암기 현황" style={{backgroundColor:'rgb(76,175,80)'}}>
+            <div style={styles.wrapper}>
+              <h2 style={styles.headline}>완전히 기억하고 있는 단어</h2>
+              <h2 style={styles.word}>word</h2>
+            </div>
+            <div style={styles.wrapper}>
+              <h2 style={styles.headline}>1회 학습한 단어</h2>
+              <h2 style={styles.word}>word</h2>
+            </div>        
+          </Tab>      
+        </Tabs>
+      </MuiThemeProvider>
+    );
+  }
+}
